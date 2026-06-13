4e472d494d8f5565e295a02a45e433da68247fc3
abhinavpampana45 <90972507+abhinavpampana45@users.noreply.github.com>
2026-06-13 18:07:21 +0530

    Create Seat.java

---
+++ src/main/java/com/example/makeyourtrip/Models/Seat.java
@@ -0,0 +1,34 @@
+package com.example.makeyourtrip.Models;
+
+
+import com.example.makeyourtrip.Enums.SeatType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "seats")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Seat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer seatId;
+
+    private String seatNo;
+
+    @Enumerated(value = EnumType.STRING)
+    private SeatType seatType;
+
+    private Integer price;
+
+    @ManyToOne
+    @JoinColumn
+    private Transport transport;
+
+}

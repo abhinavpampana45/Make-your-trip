ad7a30d1ba1c75570ee71d13bec50538edb89fd6
abhinavpampana45 <90972507+abhinavpampana45@users.noreply.github.com>
2026-06-13 18:17:15 +0530

    Create FlightResult.java

---
+++ src/main/java/com/example/makeyourtrip/ResponseDtos/FlightResult.java
@@ -0,0 +1,22 @@
+package com.example.makeyourtrip.ResponseDtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class FlightResult {
+
+    private LocalDate journeyDate;
+    private LocalTime startTime;
+    private double journeyTime;
+    private String companyName;
+    private String listOfStopInBetween;
+}
